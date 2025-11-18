3df609fb573963f0e2002a2fe845992f94132764
Yasna-a56 <nikola5606yasi@gmail.com>
2025-11-18 23:25:52 +0330

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+
+# mysql
+DATABASE_URL = "sqlite:///database.db"
+
+engine= create_engine(DATABASE_URL,echo=False, future=True)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine , autoflash=False, autocommit=False,future=True )
+
+
+# helper
+
+def get_session():
+    return SessionLocal()
---
+++ Model.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from Database import Base
+
+
+#-------Customers-------
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True)
+
+    # -------relationships-------
+    accounts = relationship("Account", back_populates="customers")
+    transactions = relationship("Transaction", back_populates="customers")
+
+#-------Accounts-------
+class Account(Base):
+    __tablename__ = "accounts"
+    id = Column(Integer, primary_key=True)
+    balance = Column(Float, default=0.0)  # mojodi , 00
+    type = Column(String, default="standard")  # 'standard', 'foreign', 'crypto'
+    pin = Column(String, nullable=False)  # pin kodom account ro khod kon
+
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+
+    #-------relationships-------
+    customer = relationship("Customer", back_populates="accounts")
+    transactions = relationship("Transaction", back_populates="accounts")
+
+#-------Transactions-------
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    amount = Column(Float, nullable=False)
+    type = Column(String, nullable=False) # transfer --> deposit, withdraw
+    description = Column(String, default="")
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    account_id = Column(Integer, ForeignKey("accounts.id"))
+
+    # -------relationships-------
+    customer = relationship("Customer", back_populates="transactions")
+    accounts = relationship("Account", back_populates="transactions")
