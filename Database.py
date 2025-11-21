4e154e59a4036cb9405938c2478070d58b14c55b
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-11-21 22:02:45 +0100

    Format Database.py for consistency

--- Database.py
+++ Database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-
 # sqlite
 DATABASE_URL = "sqlite:///database.db"
 
@@ -13,7 +12,7 @@ SessionLocal = sessionmaker(bind=engine , autoflash=False, autocommit=False,futu
 
 
 # helper
-
 def get_session():
 
     return SessionLocal()
+
