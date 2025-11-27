1a9d5dd0b2ea82c2b35d9dad7a73440ed60ddb85
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-11-27 01:07:01 +0100

    Update Database.py

--- Database.py
+++ Database.py
@@ -8,11 +8,11 @@ engine= create_engine(DATABASE_URL,echo=False, future=True)
 
 Base = declarative_base()
 
-SessionLocal = sessionmaker(bind=engine , autoflash=False, autocommit=False,future=True )
+SessionLocal = sessionmaker(bind=engine , autoflash=False, autocommit=False , future=True )
 
 
-# helper
+# helper function
 def get_session():
-
     return SessionLocal()
+
 
