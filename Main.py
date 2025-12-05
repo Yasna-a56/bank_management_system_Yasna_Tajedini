5d51085e733e3147ac48a87794df4d9f4c979248
Yasna-a56 <nikola5606yasi@gmail.com>
2025-12-06 00:30:29 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
+from Database import Base, engine
+from Model import Customer, Account, Transaction
 from Core import AdminPanel
 from Admin_GUI import AdminGUI
 
+
+Base.metadata.create_all(engine)
+
 admin_panel = AdminPanel()
 app = AdminGUI(admin_panel)
+
