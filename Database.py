46c4acce4cecd34e610a1dafa3606199b4bdb2ca
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-10 20:04:31 +0100

    Reformat Database.py for consistency

--- Database.py
+++ Database.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 DATABASE_URL = "sqlite:///database.db"
 
 engine= create_engine(DATABASE_URL,echo=False, future=True)
-
 Base = declarative_base()
 
 SessionLocal = sessionmaker(bind=engine , autoflush=False, autocommit=False,future=True )
@@ -14,3 +13,4 @@ SessionLocal = sessionmaker(bind=engine , autoflush=False, autocommit=False,futu
 def get_session():
 
     return SessionLocal()
+
