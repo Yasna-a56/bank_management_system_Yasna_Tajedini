9d31ffcd3ede3f8ecfd4f153fb3a348ac8e00f40
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-10 20:04:40 +0100

    Reformat Main.py for consistency

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ from Model import Customer, Account, Transaction
 from Core import AdminPanel
 from Admin_GUI import AdminGUI
 
-
 Base.metadata.create_all(engine)
 
 admin_panel = AdminPanel()
 app = AdminGUI(admin_panel)
+
 
